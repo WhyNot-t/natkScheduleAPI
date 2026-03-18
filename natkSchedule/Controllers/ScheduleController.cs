eb89de1a8392d5c9ddb97ea4acd2bf8f68ada7be
Denis <klychnicovden@gmail.com>
2026-03-18 15:26:07 +0300

    add controllers class

---
+++ natkSchedule/Controllers/ScheduleController.cs
@@ -0,0 +1,25 @@
+﻿using natkSchedule.Data;
+using natkSchedule.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace natkSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
+            return Ok(result);
+        }
+    }
+}
