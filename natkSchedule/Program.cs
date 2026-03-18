c93077335342eef97d47355e30f129b3d3852c94
Denis <klychnicovden@gmail.com>
2026-03-18 14:58:40 +0300

    updating the code in program.cs

--- natkSchedule/Program.cs
+++ natkSchedule/Program.cs
@@ -1,4 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using natkSchedule.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+DotNetEnv.Env.Load();
+
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                       $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                       $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                       $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+
+$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 // Add services to the container.
 
@@ -19,6 +34,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
